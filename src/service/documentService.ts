7f8233ef027deee6a058f07700438a6a210a65fa
cweijan <cweijan@163.com>
2020-11-17 11:03:03 +0800

    implment open document

--- src/service/documentService.ts
+++ src/service/documentService.ts
@@ -1,7 +1,13 @@
-export class DocumentService{
-    
-    public static open(): any {
-        throw new Error("Method not implemented.");
+import { ConfigKey, Global } from "../common/global";
+import { Process } from "../common/processWrapper";
+
+export class DocumentService {
+
+    public static open(): void {
+
+        const documentPath = Global.getConfig(ConfigKey.documentPath)
+        Process.exec(`C:/Windows/hh.exe ${documentPath}`)
+
     }
-    
+
 }
