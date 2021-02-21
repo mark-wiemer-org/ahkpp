5d97ffcfd02765eb16c3882b8f1e248b1830c85d
Mark Wiemer <markwiemer@outlook.com>
2021-02-21 15:05:25 -0800

    Refactor format tests

--- src/test/suite/format/format.test.ts
+++ src/test/suite/format/format.test.ts
@@ -3,14 +3,19 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as vscode from 'vscode';
 import { FormatProvider } from '../../../providers/formattingProvider';
-// import * as myExtension from '../../extension';
 
-/** Unformatted input file */
-const inFilename = 'demo.in.ahk';
-/** Formatted output file */
-const outFilename = 'demo.out.ahk';
+interface FormatTest {
+    filenameRoot: string;
+    options: vscode.FormattingOptions;
+}
 
-/** Path to the directory holding the formatting samples */
+const formatTests: FormatTest[] = [
+    { filenameRoot: 'demo', options: { tabSize: 4, insertSpaces: true } },
+    { filenameRoot: 'multiline', options: { tabSize: 4, insertSpaces: false } },
+];
+const inFilenameSuffix = '.in.ahk';
+const outFilenameSuffix = '.out.ahk';
+
 const filesParentPath = path.join(
     __dirname,
     '..',
@@ -24,53 +29,31 @@ const filesParentPath = path.join(
 );
 
 suite('Formatter', () => {
-    test('Format test', async () => {
-        const outFileString = fs
-            .readFileSync(path.join(filesParentPath, outFilename))
-            .toString();
-
-        const unformattedSampleFile = await vscode.workspace.openTextDocument(
-            path.join(filesParentPath, inFilename),
-        );
-        const textEditor = await vscode.window.showTextDocument(
-            unformattedSampleFile,
-        );
-        const formatter = new FormatProvider();
-        const edits = formatter.provideDocumentFormattingEdits(
-            unformattedSampleFile,
-            { tabSize: 4, insertSpaces: true },
-            null,
-        );
-        await textEditor.edit((editBuilder) => {
-            edits.forEach((edit) =>
-                editBuilder.replace(edit.range, edit.newText),
+    formatTests.forEach((formatTest) => {
+        test(`Format ${formatTest.filenameRoot}`, async () => {
+            const inFilename = formatTest.filenameRoot + inFilenameSuffix;
+            const outFilename = formatTest.filenameRoot + outFilenameSuffix;
+            const outFileString = fs
+                .readFileSync(path.join(filesParentPath, outFilename))
+                .toString();
+            const unformattedSampleFile = await vscode.workspace.openTextDocument(
+                path.join(filesParentPath, inFilename),
             );
-        });
-        assert.strictEqual(textEditor.document.getText(), outFileString);
-    });
-
-    test('Respect insertSpaces option test', async () => {
-        const outFileString = fs
-            .readFileSync(path.join(filesParentPath, 'multiline.out.ahk'))
-            .toString();
-
-        const unformattedSampleFile = await vscode.workspace.openTextDocument(
-            path.join(filesParentPath, 'multiline.in.ahk'),
-        );
-        const textEditor = await vscode.window.showTextDocument(
-            unformattedSampleFile,
-        );
-        const formatter = new FormatProvider();
-        const edits = formatter.provideDocumentFormattingEdits(
-            unformattedSampleFile,
-            { tabSize: 4, insertSpaces: false },
-            null,
-        );
-        await textEditor.edit((editBuilder) => {
-            edits.forEach((edit) =>
-                editBuilder.replace(edit.range, edit.newText),
+            const textEditor = await vscode.window.showTextDocument(
+                unformattedSampleFile,
             );
+            const formatter = new FormatProvider();
+            const edits = formatter.provideDocumentFormattingEdits(
+                unformattedSampleFile,
+                formatTest.options,
+                null,
+            );
+            await textEditor.edit((editBuilder) => {
+                edits.forEach((edit) =>
+                    editBuilder.replace(edit.range, edit.newText),
+                );
+            });
+            assert.strictEqual(textEditor.document.getText(), outFileString);
         });
-        assert.strictEqual(textEditor.document.getText(), outFileString);
     });
 });
