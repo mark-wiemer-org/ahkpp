234dd91cfecc698af2674ac796d8ed2b69244138
Mark Wiemer <markwiemer@outlook.com>
2021-05-09 13:53:38 -0700

    Simplify docs for hasMoreCloseParens

--- src/providers/formattingProvider.utils.ts
+++ src/providers/formattingProvider.utils.ts
@@ -1,6 +1,4 @@
-/**
- * Returns true if this line has more closing parens than opening parens (round brackets).
- */
+/** @return true iff this line has more closing parens than opening parens (round brackets) */
 export function hasMoreCloseParens(line: string): boolean {
     if (!line.includes(')')) return false;
     const openCount = line.match(/\(/g)?.length ?? 0;
