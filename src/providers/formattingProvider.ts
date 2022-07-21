cf629d98c4a945b8e72f6a1b1830f0fc653cc3d4
kyklish <kyklish@gmail.com>
2022-07-21 21:03:26 +0300

    allow close brace

--- src/providers/formattingProvider.ts
+++ src/providers/formattingProvider.ts
@@ -222,8 +222,9 @@ export class FormatProvider implements vscode.DocumentFormattingEditProvider {
                     if (
                         // if the regex matches the purified line
                         (temp = new RegExp(
-                            // no words before 'one command code'
-                            '^\\s*' + oneCommand + '\\b(.*)',
+                            // before 'one command code' allowed only close brace
+                            // example: '} else' or '} if'
+                            '^}\\s*' + oneCommand + '\\b(.*)',
                         ).exec(purifiedLine)) &&
                         // and the captured group not includes a slash
                         !temp[1].includes('/')
