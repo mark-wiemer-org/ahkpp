94d6868b0ecee8280f90fd3c1152fd17f6c3afc5
kyklish <kyklish@gmail.com>
2022-07-22 19:53:13 +0300

    initial commit

--- src/test/suite/format/format.test.ts
+++ src/test/suite/format/format.test.ts
@@ -21,6 +21,7 @@ const formatTests: FormatTest[] = [
     { filenameRoot: '25-multiline-string' },
     { filenameRoot: '58-parentheses-indentation' },
     { filenameRoot: '72-paren-hotkey' },
+    { filenameRoot: '189-space-at-end-of-line' },
     { filenameRoot: 'ahk-explorer' },
     { filenameRoot: 'demo' },
     {
