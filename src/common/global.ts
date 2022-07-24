6335991412a5a77869c94a4eb7702141b767538a
kyklish <kyklish@gmail.com>
2022-07-24 09:57:49 +0300

    move settings to separate group

--- src/common/global.ts
+++ src/common/global.ts
@@ -31,5 +31,5 @@ export enum ConfigKey {
     helpPath = 'helpPath',
     enableIntellisense = 'enableIntellisense',
     executePath = 'executePath',
-    allowedNumberOfEmptyLines = 'allowedNumberOfEmptyLines',
+    allowedNumberOfEmptyLines = 'formatter.allowedNumberOfEmptyLines',
 }
