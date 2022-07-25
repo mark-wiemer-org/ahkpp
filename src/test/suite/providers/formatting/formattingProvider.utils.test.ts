0516509f21bb9f6c14905cbad71f2b9488339dc0
kyklish <kyklish@gmail.com>
2022-07-25 20:08:36 +0300

    add unit test

--- src/test/suite/providers/formatting/formattingProvider.utils.test.ts
+++ src/test/suite/providers/formatting/formattingProvider.utils.test.ts
@@ -2,6 +2,7 @@ import * as assert from 'assert';
 import {
     hasMoreCloseParens,
     hasMoreOpenParens,
+    removeEmptyLines,
 } from '../../../../providers/formattingProvider.utils';
 
 suite('FormattingProvider utils', () => {
@@ -72,6 +73,52 @@ suite('FormattingProvider utils', () => {
             test("'" + data.in + "'" + ' => ' + data.rs.toString(), () => {
                 assert.strictEqual(hasMoreOpenParens(data.in), data.rs);
             });
+        });
+    });
+
+    suite('removeEmptyLines', () => {
+        // List of test data
+        let dataList = [
+            // {
+            //     in: , // input test string
+            //     ln: , // allowed empty lines
+            //     rs: , // expected result
+            // },
+            {
+                in: '\ntext\n\n\n\ntext\n\n\n\n',
+                ln: 1,
+                rs: 'text\n\ntext\n\n',
+            },
+            {
+                in: '\ntext\n\n\n\ntext\n\n\n\n',
+                ln: 2,
+                rs: 'text\n\n\ntext\n\n\n',
+            },
+            {
+                in: 'text\ntext',
+                ln: 1,
+                rs: 'text\ntext',
+            },
+            {
+                in: 'text\n',
+                ln: 1,
+                rs: 'text\n',
+            },
+        ];
+        dataList.forEach((data) => {
+            test(
+                "'" +
+                    data.in.replace(/\n/g, '\\n') +
+                    "' => '" +
+                    data.rs.replace(/\n/g, '\\n') +
+                    "'",
+                () => {
+                    assert.strictEqual(
+                        removeEmptyLines(data.in, data.ln),
+                        data.rs,
+                    );
+                },
+            );
         });
     });
 });
