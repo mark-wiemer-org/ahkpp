d24fe03c3e48f33a88136f4b760ce8d4bae303c5
kyklish <kyklish@gmail.com>
2022-09-06 19:58:25 +0300

    test CodeUtil.purify()

---
+++ src/test/suite/common/codeUtil.test.ts
@@ -0,0 +1,43 @@
+import * as assert from 'assert';
+import { CodeUtil } from '../../../common/codeUtil';
+
+suite('Code utils', () => {
+    suite('purify', () => {
+        // List of test data
+        let dataList = [
+            // {
+            //     in: , // input test string
+            //     rs: , // expected result
+            // },
+            {
+                in: 'a := 5 ; comment',
+                rs: 'a := 5 ',
+            },
+            {
+                in: 'str := "string"',
+                rs: 'str := ""',
+            },
+            {
+                in: 'b := {str: "object"}',
+                rs: 'b := ',
+            },
+            {
+                in: 'str = legacy    text',
+                rs: 'str = legacy text',
+            },
+            {
+                in: 'Gui, %id%: Color, % color',
+                rs: '',
+            },
+            {
+                in: 'MsgBox % str . (var + 1)',
+                rs: 'MsgBox str . (var + 1)',
+            },
+        ];
+        dataList.forEach((data) => {
+            test("'" + data.in + "' => '" + data.rs + "'", () => {
+                assert.strictEqual(CodeUtil.purify(data.in), data.rs);
+            });
+        });
+    });
+});
