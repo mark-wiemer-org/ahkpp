86a76760455e924e85b9af673f0423c1120047fb
cweijan <cweijan@163.com>
2022-09-25 12:42:20 +0800

    Remove unnecessary esling rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,6 @@ module.exports = {
     },
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-empty-function': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/ban-types': 'off',
-      '@typescript-eslint/no-this-alias': 'off',
-      '@typescript-eslint/no-non-null-assertion': 'off',
     },
   };
