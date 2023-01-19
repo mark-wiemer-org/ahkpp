0485455ec91c9abd3b9189607e312178a0d1b3e0
Kyklish <kyklish@gmail.com>
2023-01-19 18:25:31 +0200

    Add IfMsgBox command to one command code list (#307)
    Co-authored-by: Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
    Closes https://github.com/vscode-autohotkey/ahkpp/issues/290

--- src/test/suite/format/format.test.ts
+++ src/test/suite/format/format.test.ts
@@ -65,6 +65,7 @@ const formatTests: FormatTest[] = [
     { filenameRoot: '255-style-k-and-r' },
     { filenameRoot: '255-style-mix' },
     { filenameRoot: '255-style-one-true-brace' },
+    { filenameRoot: '290-ifmsgbox' },
     { filenameRoot: '291-single-line-comment' },
     { filenameRoot: '316-if-object-continuation-section' },
     { filenameRoot: 'ahk-explorer' },
