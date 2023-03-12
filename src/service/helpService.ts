ba70a55217b5c273b6d90e5a6238a1cc3d5cf676
Mark Wiemer <markwiemer@outlook.com>
2023-03-12 11:09:14 -0700

    POC: Open tutorial page instead of default path

--- src/service/helpService.ts
+++ src/service/helpService.ts
@@ -4,6 +4,6 @@ import { Process } from '../common/processWrapper';
 export class HelpService {
     public static open(): void {
         const helpPath = Global.getConfig(ConfigKey.helpPath);
-        Process.exec(`C:/Windows/hh.exe ${helpPath}`);
+        Process.exec(`C:/Windows/hh.exe ${helpPath}::/docs/Tutorial.htm`);
     }
 }
