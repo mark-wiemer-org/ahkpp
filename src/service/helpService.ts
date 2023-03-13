e69d53298c15ddce90d68aa4172f69062c1bc0c3
Mark Wiemer <markwiemer@outlook.com>
2023-03-12 20:59:46 -0700

    Remove `HelpService` class, replace with function

--- src/service/helpService.ts
+++ src/service/helpService.ts
@@ -1,13 +1,11 @@
 import { ConfigKey, Global } from '../common/global';
 import { Process } from '../common/processWrapper';
-import { getSelectedText } from '@/common/codeUtil';
+import { getSelectedText } from '../common/codeUtil';
 import * as vscode from 'vscode';
 
-export class HelpService {
-    public static open(): void {
-        const text = getSelectedText(vscode.window.activeTextEditor);
-        vscode.window.showInformationMessage(`Selection: "${text}"`);
-        const helpPath = Global.getConfig(ConfigKey.helpPath);
-        Process.exec(`C:/Windows/hh.exe ${helpPath}::/docs/Tutorial.htm`);
-    }
-}
+export const openHelp = (): void => {
+    const text = getSelectedText(vscode.window.activeTextEditor);
+    vscode.window.showInformationMessage(`Selection: "${text}"`);
+    const helpPath = Global.getConfig(ConfigKey.helpPath);
+    Process.exec(`C:/Windows/hh.exe ${helpPath}::/docs/Tutorial.htm`);
+};
