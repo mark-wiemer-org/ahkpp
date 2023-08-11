002c8409280dc67949754f39c2a70c5a7849bb78
Mark Wiemer <markwiemer@outlook.com>
2023-08-11 16:52:46 -0700

    Remove non-formatter capabilities

--- src/extension.ts
+++ src/extension.ts
@@ -1,97 +1,12 @@
 import * as vscode from 'vscode';
-import { ProviderResult } from 'vscode';
-import { Parser } from './parser/parser';
-import { RunnerService } from './service/runnerService';
-import { DebugSession } from './debugger/debugSession';
-import { DefProvider } from './providers/defProvider';
-import { createEditorListener } from './service/templateService';
 import { FormatProvider } from './providers/formattingProvider';
-import { SymbolProvider } from './providers/symbolProvider';
-import { FileManager } from './common/fileManager';
-import { AhkHoverProvider } from './providers/ahkHoverProvider';
-import { RefProvider } from './providers/refProvider';
-import { Global, ConfigKey } from './common/global';
-import { AhkRenameProvider } from './providers/ahkRenameProvider';
-import { SignatureProvider } from './providers/signatureProvider';
-import { CompletionProvider } from './providers/completionProvider';
-import { openHelp } from './service/help/helpService';
 
 export function activate(context: vscode.ExtensionContext) {
-    (async () => {
-        Global.updateStatusBarItems('Indexing AutoHotkey Workspace...');
-        await Parser.buildByPath(vscode.workspace.rootPath);
-        Global.updateStatusBarItems('Index Workspace Success!');
-        Global.hide();
-    })();
-
     const language = { language: 'ahk' };
-    FileManager.init(context);
     context.subscriptions.push(
-        vscode.languages.registerHoverProvider(
-            language,
-            new AhkHoverProvider(context),
-        ),
-        vscode.languages.registerDefinitionProvider(
-            language,
-            new DefProvider(),
-        ),
-        vscode.languages.registerRenameProvider(
-            language,
-            new AhkRenameProvider(),
-        ),
-        vscode.languages.registerSignatureHelpProvider(
-            language,
-            new SignatureProvider(),
-            '(',
-            ',',
-        ),
-        vscode.languages.registerDocumentSymbolProvider(
-            language,
-            new SymbolProvider(),
-        ),
         vscode.languages.registerDocumentFormattingEditProvider(
             language,
             new FormatProvider(),
         ),
-        vscode.languages.registerReferenceProvider(language, new RefProvider()),
-        vscode.debug.registerDebugAdapterDescriptorFactory(
-            'ahk',
-            new InlineDebugAdapterFactory(),
-        ),
-        createEditorListener(),
-        vscode.commands.registerCommand('ahk++.compile', () =>
-            RunnerService.compile(false),
-        ),
-        vscode.commands.registerCommand('ahk++.compilerGui', () =>
-            RunnerService.compile(true),
-        ),
-        vscode.commands.registerCommand('ahk++.debug', () =>
-            RunnerService.startDebugger(),
-        ),
-        vscode.commands.registerCommand('ahk++.openHelp', openHelp),
-        vscode.commands.registerCommand('ahk++.run', () => RunnerService.run()),
-        vscode.commands.registerCommand('ahk++.runSelection', () =>
-            RunnerService.runSelection(),
-        ),
     );
-
-    if (Global.getConfig<boolean>(ConfigKey.enableIntellisense)) {
-        context.subscriptions.push(
-            vscode.languages.registerCompletionItemProvider(
-                language,
-                new CompletionProvider(),
-                '.',
-            ),
-        );
-    }
-}
-
-class InlineDebugAdapterFactory
-    implements vscode.DebugAdapterDescriptorFactory
-{
-    public createDebugAdapterDescriptor(
-        _session: vscode.DebugSession,
-    ): ProviderResult<vscode.DebugAdapterDescriptor> {
-        return new vscode.DebugAdapterInlineImplementation(new DebugSession());
-    }
 }
