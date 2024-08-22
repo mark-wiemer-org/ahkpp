b95f8a33bb308bc5b9623f70168092d0b94b6f97
Mark Wiemer <markwiemer@outlook.com>
2024-08-21 21:36:23 -0700

    Fixup nested eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,5 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
-    { ignores: ['out', 'dist', '**/*.d.ts'] },
+    { ignores: ['out', 'dist', '**/*.d.ts', 'ahk2'] },
 );
