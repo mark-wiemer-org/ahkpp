57a751302817d7fd313d6e80a18170036a38d841
Mark Wiemer <markwiemer@outlook.com>
2024-09-21 18:18:47 -0700

    Add test util showDocument

--- src/test/utils.ts
+++ src/test/utils.ts
@@ -7,6 +7,11 @@ import * as vscode from 'vscode';
 export const getDocument = async (path: string): Promise<vscode.TextDocument> =>
     await vscode.workspace.openTextDocument(path);
 
+/** Shows the doc and returns the TextEditor for the doc */
+export const showDocument = async (
+    doc: vscode.TextDocument,
+): Promise<vscode.TextEditor> => await vscode.window.showTextDocument(doc);
+
 // Copying to test helpers for easy import
 export async function sleep(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
